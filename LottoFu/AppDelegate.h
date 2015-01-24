58b28d404e955fb70e1cb52f87a14ee3fa31a91e
Wayne O. Cochran <wcochran@vancouver.wsu.edu>
2015-01-24 08:34:42 -0800

    Initial Commit

---
+++ LottoFu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LottoFu
+//
+//  Created by Wayne Cochran on 1/24/15.
+//  Copyright (c) 2015 WSUV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LottoFu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LottoFu
+//
+//  Created by Wayne Cochran on 1/24/15.
+//  Copyright (c) 2015 WSUV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
